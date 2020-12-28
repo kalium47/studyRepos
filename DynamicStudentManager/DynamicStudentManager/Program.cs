465eea42296fea9b3160bedd471bcf81be45526c
Kalium47 <kalium47@hotmail.com>
2020-12-28 16:33:29 +0000

    N/A

---
+++ DynamicStudentManager/DynamicStudentManager/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DynamicStudentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var studentNameList = new List<string>();
+            var studentGradeList = new List<int>();
+
+            var addOne = true;
+
+            while(addOne)
+            {
+                Console.WriteLine("Name?");
+                studentNameList.Add(Console.ReadLine());
+
+                Console.WriteLine("Grade?");
+                studentGradeList.Add(int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Add Another? Y/N");
+
+                if (Console.ReadLine() != "y")
+                    addOne = false;
+            }
+
+            for (int i = 0; i < studentNameList.Count; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade {1}", studentNameList[i], studentGradeList[i]);
+            }            
+        }
+    }
+}
---
+++ StudentManager/StudentManager/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StudentManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many students?");
+            var studentCount = int.Parse(Console.ReadLine());
+
+            var studentNameArray = new string[studentCount];
+            var studentGradeArray = new int[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name?");
+                studentNameArray[i] = Console.ReadLine();
+
+                Console.WriteLine("Grade?");
+                studentGradeArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                Console.WriteLine("Name: {0}, Grade {1}", studentNameArray[i], studentGradeArray[i]);
+            }
+
+        }
+
+
+    }
+}
