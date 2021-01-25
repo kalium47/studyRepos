c038aa304bd78106a45edd2855116d83af5a0480
Kalium47 <kalium47@hotmail.com>
2021-01-25 15:24:24 +0000

    Added Object-oriented Class

--- DynamicStudentManager/DynamicStudentManager/Program.cs
+++ DynamicStudentManager/DynamicStudentManager/Program.cs
@@ -8,18 +8,30 @@ namespace DynamicStudentManager
         static void Main(string[] args)
         {
 
-            var studentNameList = new List<string>();
-            var studentGradeList = new List<int>();
+            var students = new List<StudentInfo>();
 
             var addOne = true;
 
             while(addOne)
             {
+                var newStudentInfo = new StudentInfo();
+
                 Console.WriteLine("Name?");
-                studentNameList.Add(Console.ReadLine());
+               newStudentInfo.Name = (Console.ReadLine());
 
                 Console.WriteLine("Grade?");
-                studentGradeList.Add(int.Parse(Console.ReadLine()));
+                newStudentInfo.Grade = (int.Parse(Console.ReadLine()));
+
+                Console.WriteLine("Birthday?");
+                newStudentInfo.Birthday = (Console.ReadLine());
+
+                Console.WriteLine("Address?");
+                newStudentInfo.Address = (Console.ReadLine());
+
+                Console.WriteLine("PhoneNumber?");
+                newStudentInfo.PhoneNumber = (int.Parse(Console.ReadLine()));
+
+                students.Add(newStudentInfo);
 
                 Console.WriteLine("Add Another? Y/N");
 
@@ -30,10 +42,21 @@ namespace DynamicStudentManager
                     addOne = false;
             }
 
-            for (int i = 0; i < studentNameList.Count; i++)
+            foreach (var student in students)
             {
-                Console.WriteLine("Name: {0}, Grade {1}", studentNameList[i], studentGradeList[i]);
-            }            
+                Console.WriteLine("Name: {0}, Grade {1}, Birthday {2}, Address {3}, PhoneNumber {4}. ", student.Name, student.Grade, student.Birthday, student.Address, student.PhoneNumber);
+            }
+
         }
+    }
+
+    class StudentInfo
+    {
+        public string Name;
+        public int Grade;
+        public string Birthday;
+        public string Address;
+        public int PhoneNumber;
+
     }
 }
